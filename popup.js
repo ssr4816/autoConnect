e12f1c7d27654c07c96a4999115a5c749a5ec9e5
sisir <sisirrag500@gmail.com>
2023-02-23 00:36:22 +0530

    Final commit

---
+++ popup.js
@@ -0,0 +1,21 @@
+document.getElementById("submit").addEventListener("click", connect)
+async function connect() {
+    const [tab] = await chrome.tabs.query({ // to get current active tab
+        active: true,
+        currentWindow: true,
+    });
+    await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: () => {
+            var buttons = document.getElementsByClassName("artdeco-button artdeco-button--2 artdeco-button--secondary ember-view"); // get buttons
+            for (let i = 0; i < buttons.length; i++) {
+                setTimeout(()=>{
+                    if (buttons[i].innerText === 'Connect'){ //check if its a connect button
+                        buttons[i].click(); // click on connect button
+                        document.getElementsByClassName("artdeco-button artdeco-button--2 artdeco-button--primary ember-view ml1")[0].click() // click on send button
+                    } 
+                },500);                
+            }
+        },
+    });
+}
